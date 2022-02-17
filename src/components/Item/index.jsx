98434cc1e5ee938ce29512abd722a51c2f25c4ef
Danylo Prudnikov <danylo.prudnikov@gmail.com>
2022-02-17 21:15:50 +0300

    refactoring

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
+import React from "react";
 import "./style.scss";
-import Stack from '@mui/material/Stack';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const TodoItem = ({todo, editTodo, removeTodo}) => {
+const TodoItem = ({ todo, editTodo, removeTodo }) => {
+  const handleChange = () => {
+    todo.completed = !todo.completed;
+    editTodo(todo);
+  };
 
-    const handleChange = () => {
-        todo.completed = !todo.completed;
-        editTodo(todo);
-    };
+  const handleDelete = () => {
+    removeTodo(todo.id);
+  };
 
-    const handleDelete = () => {
-        removeTodo(todo.id);
-    };
-
-    return (
-        <li className='item'>
-            <input type="checkbox" checked = {todo.completed} onChange={handleChange}/>
-            <p className={`${todo.completed ? "completed" : ""}`}>{todo.title}</p>   
-            <Stack>
-                <IconButton aria-label="delete">
-                    <DeleteIcon  className='btn-del' onClick={handleDelete}/>
-                </IconButton>
-            </Stack>
-            {/* <div className='btn-del' onClick={handleDelete}>x</div> */}
-        </li>
-    )
-}
+  return (
+    <li className="item">
+      <input type="checkbox" checked={todo.completed} onChange={handleChange} />
+      <p className={`${todo.completed ? "completed" : ""}`}>{todo.title}</p>
+      <Stack>
+        <IconButton aria-label="delete">
+          <DeleteIcon className="btn-del" onClick={handleDelete} />
+        </IconButton>
+      </Stack>
+      {/* <div className='btn-del' onClick={handleDelete}>x</div> */}
+    </li>
+  );
+};
 
 export default TodoItem;
-
-
